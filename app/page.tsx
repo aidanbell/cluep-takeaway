f5ac4d59e9de53d37c4118544a22b1de2690693a
Aidan Bell <aidanbell0182@gmail.com>
2024-09-03 11:18:50 -0400

    [UI] Wireframe complete without hoverstates or interactivity

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
-import { SearchIcon, PlusIcon } from "./ui/Icons";
+import { SearchIcon, PlusIcon, SendMessageIcon, ChevronRightIcon } from "./ui/Icons";
 
 export default function Home() {
   return (
-    <main className="flex flex-col mx-auto mt-auto mb-0 h-600 center justify-between rounded-lg border border-gray-300">
+    <main className="flex flex-col mx-auto mt-auto mb-0 center justify-between rounded-lg border border-gray-300">
+      <SearchIcon size={30} className="search-icon text-gray-300" />
+      <ChevronRightIcon size={30} className="menu-open-icon text-gray-300" />
       <div className="content flex flex-col">
-        <SearchIcon size={50} className="text-gray-300" />
 
         <div className="messages"></div>
         <div className="tasks flex flex-row justify-between text-gray-300">
@@ -24,12 +25,13 @@ export default function Home() {
           </button>
         </div>
         <div className="send-message w-full rounded-lg border border-gray-300 flex flex-row justify-between items-center text-gray-300">
-          <PlusIcon size={50} className="text-gray-300" />
+          <PlusIcon size={30} className="text-gray-300" />
           <input
             type="text"
             className="block w-full"
             placeholder="What's on your mind?"
           />
+          <SendMessageIcon size={40} className="text-gray-300" />
         </div>
       </div>
     </main>
--- app/ui/Icons.tsx
+++ app/ui/Icons.tsx
@@ -23,10 +23,53 @@ export function SearchIcon({ className, size }: { className: string, size: numbe
 
 export function PlusIcon({ className, size }: { className: string, size: number }) {
   return (
-    <PlusSVG
+    <svg
       className={className}
-      width={size}
+      xmlns="http://www.w3.org/2000/svg"
       height={size}
-    />
+      width={size}
+      fill="currentColor"
+      viewBox="0 0 24 24">
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M18 13h-5v5c0 .55-.45 1-1 1s-1-.45-1-1v-5H6c-.55 0-1-.45-1-1s.45-1 1-1h5V6c0-.55.45-1 1-1s1 .45 1 1v5h5c.55 0 1 .45 1 1s-.45 1-1 1z" />
+    </svg>
+  );
+}
+
+export function SendMessageIcon({ className, size }: { className: string, size: number }) {
+  return (
+    <svg
+      className={className}
+      fill="currentColor"
+      height={size}
+      width={size}
+      version="1.1"
+      id="Icons"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="-3.2 -3.2 38.40 38.40">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M21,2H11c-5,0-9,4-9,9v10c0,5,4,9,9,9h10c5,0,9-4,9-9V11C30,6,26,2,21,2z M20.7,15.7C20.5,15.9,20.3,16,20,16 s-0.5-0.1-0.7-0.3L17,13.4V22c0,0.6-0.4,1-1,1s-1-0.4-1-1v-8.6l-2.3,2.3c-0.4,0.4-1,0.4-1.4,0s-0.4-1,0-1.4l4-4 c0.1-0.1,0.2-0.2,0.3-0.2c0.2-0.1,0.5-0.1,0.8,0c0.1,0.1,0.2,0.1,0.3,0.2l4,4C21.1,14.7,21.1,15.3,20.7,15.7z"></path>
+      </g>
+    </svg>
+  );
+}
+
+export function ChevronRightIcon({ className, size }: { className: string, size: number }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      height={size}
+      width={size}
+      viewBox="0 0 24 24"
+      fill="currentColor">
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M9.29 6.71c-.39.39-.39 1.02 0 1.41L13.17 12l-3.88 3.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41L10.7 6.7c-.38-.38-1.02-.38-1.41.01z" />
+    </svg>
   );
 }
