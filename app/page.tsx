c7e476d9abc583b1ae34dc592a98db6acb56b449
Aidan Bell <aidanbell0182@gmail.com>
2024-09-03 14:30:39 -0400

    [UI] Dynamic ToolTip Component added. Needs some fine-tuning. Dummy message hardcoded to begin styling messages.

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,23 @@ export default function Home() {
   return (
     <main className="flex flex-row relative mx-auto mt-auto mb-0 center justify-between rounded-lg border border-gray-300">
       <Sidenav />
-      <SearchIcon size={30} className="search-icon text-gray-300" />
+      <SearchIcon size={30} className="text-gray-300 hover:text-black" />
       <div className="content flex flex-col shrink">
 
-        <div className="messages"></div>
+        <div className="messages flex items-end justify-end">
+          <div className="message flex items-baseline justify-end">
+            <div className="bg-blue-400 p-4 my-6 rounded-lg flex-1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+              optio, eaque rerum!
+            </div>
+            <div className="w-3 overflow-hidden">
+              <div className="h-4 bg-blue-400 -rotate-45 transform origin-bottom-left rounded-sm"></div>
+            </div>
+          </div>
+        </div>
+
         <div className="tasks flex flex-row justify-between text-gray-300">
           <button className="task-button rounded-full border border-gray-300">
             Task
--- app/ui/Icons.tsx
+++ app/ui/Icons.tsx
@@ -2,22 +2,25 @@ import PlusSVG from "@/public/Icons/add_black_24dp.svg";
 
 export function SearchIcon({ className, size }: { className: string, size: number }) {
   return (
-    <svg
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      focusable="false">
-      <circle cx="11" cy="11" r="8"></circle>
-      <line x1="20" y1="20" x2="16.65" y2="16.65"></line>
-    </svg>
+    <div className="group flex search-icon">
+      <ToolTip text="Search" pos="right" />
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        aria-hidden="true"
+        focusable="false">
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="20" y1="20" x2="16.65" y2="16.65"></line>
+      </svg>
+    </div>
   );
 }
 
@@ -38,38 +41,88 @@ export function PlusIcon({ className, size }: { className: string, size: number 
 
 export function SendMessageIcon({ className, size }: { className: string, size: number }) {
   return (
-    <svg
-      className={className}
-      fill="currentColor"
-      height={size}
-      width={size}
-      version="1.1"
-      id="Icons"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="-3.2 -3.2 38.40 38.40">
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"></g>
-      <g id="SVGRepo_iconCarrier">
-        <path d="M21,2H11c-5,0-9,4-9,9v10c0,5,4,9,9,9h10c5,0,9-4,9-9V11C30,6,26,2,21,2z M20.7,15.7C20.5,15.9,20.3,16,20,16 s-0.5-0.1-0.7-0.3L17,13.4V22c0,0.6-0.4,1-1,1s-1-0.4-1-1v-8.6l-2.3,2.3c-0.4,0.4-1,0.4-1.4,0s-0.4-1,0-1.4l4-4 c0.1-0.1,0.2-0.2,0.3-0.2c0.2-0.1,0.5-0.1,0.8,0c0.1,0.1,0.2,0.1,0.3,0.2l4,4C21.1,14.7,21.1,15.3,20.7,15.7z"></path>
-      </g>
-    </svg>
+    <div className="group flex send-message-icon">
+      <svg
+        className={className}
+        fill="currentColor"
+        height={size}
+        width={size}
+        version="1.1"
+        id="Icons"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-3.2 -3.2 38.40 38.40">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+          <path d="M21,2H11c-5,0-9,4-9,9v10c0,5,4,9,9,9h10c5,0,9-4,9-9V11C30,6,26,2,21,2z M20.7,15.7C20.5,15.9,20.3,16,20,16 s-0.5-0.1-0.7-0.3L17,13.4V22c0,0.6-0.4,1-1,1s-1-0.4-1-1v-8.6l-2.3,2.3c-0.4,0.4-1,0.4-1.4,0s-0.4-1,0-1.4l4-4 c0.1-0.1,0.2-0.2,0.3-0.2c0.2-0.1,0.5-0.1,0.8,0c0.1,0.1,0.2,0.1,0.3,0.2l4,4C21.1,14.7,21.1,15.3,20.7,15.7z"></path>
+        </g>
+      </svg>
+      {/* <ToolTip text="Send Message" pos="top" /> */}
+    </div>
   );
 }
 
 export function ChevronRightIcon({ className, size }: { className: string, size: number }) {
   return (
-    <svg
-      className={className}
-      xmlns="http://www.w3.org/2000/svg"
-      height={size}
-      width={size}
-      viewBox="0 0 24 24"
-      fill="currentColor">
-      <path d="M0 0h24v24H0V0z" fill="none" />
-      <path d="M9.29 6.71c-.39.39-.39 1.02 0 1.41L13.17 12l-3.88 3.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41L10.7 6.7c-.38-.38-1.02-.38-1.41.01z" />
-    </svg>
+    <div className="group flex chevron-right-icon">
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        height={size}
+        width={size}
+        viewBox="0 0 24 24"
+        fill="currentColor">
+        <path d="M0 0h24v24H0V0z" fill="none" />
+        <path d="M9.29 6.71c-.39.39-.39 1.02 0 1.41L13.17 12l-3.88 3.88c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41L10.7 6.7c-.38-.38-1.02-.38-1.41.01z" />
+      </svg>
+      <ToolTip text="Open Sidebar" pos="left" />
+    </div>
+  );
+}
+// tooltip group-hover:opacity-100 transition-opacity opacity-0 
+export function ToolTip({ text, pos }: { text: string, pos: "left" | "right" | "top" | "bottom" }) {
+  function drawArrow() {
+    switch (pos) {
+      case "left":
+        return "h-4 bg-gray-800 rotate-45 transform origin-bottom-right rounded-sm";
+      case "right":
+        return "h-4 bg-gray-800 rotate-45 transform origin-top-left rounded-sm";
+      case "bottom":
+        return "h-2 bg-gray-800 rotate-45 transform origin-top-right";
+      case "top":
+        return "h-2 bg-gray-800 -rotate-45 transform origin-bottom-right";
+    }
+  }
+
+  return (
+    <div
+      className={`tooltip group-hover:opacity-100 transition-opacity opacity-0 flex ${
+        pos === "left" || pos === "right" ? "flex-row" : "flex-col"
+      } ${pos === "left" && "flex-row-reverse"} ${
+        pos === "top" && "flex-col-reverse"
+      } items-center justify-center`}>
+      <div className="bg-gray-800 p-1.5 rounded-lg flex-1 text-white">
+        {text}
+      </div>
+      <div className="w-3 overflow-hidden inline-block">
+        {/* <div className="h-4 bg-gray-800 rotate-45 transform origin-top-left rounded-sm"></div> */}
+        <div className={drawArrow()}></div>
+      </div>
+    </div>
+
+    // TOP:
+    // <div className="h-2 bg-gray-800 rotate-45 transform origin-top-right">
+
+    // BOTTOM:
+    // <div className="h-2 bg-gray-800 rotate-45 transform origin-bottom-right">
+
+    // LEFT: (Arrow pointing right)
+    // <div className="h-4 bg-gray-800 rotate-45 transform origin-bottom-right rounded-sm"></div>
+
+    // RIGHT: (Arrow pointing left)
+    // <div className="h-4 bg-gray-800 rotate-45 transform origin-top-left rounded-sm"></div>
   );
 }
