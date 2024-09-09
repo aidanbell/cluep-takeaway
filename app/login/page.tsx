1bc11890efe18eb3ad582e95a9f2ab0a9582cdef
Aidan Bell <aidanbell0182@gmail.com>
2024-09-09 12:20:31 -0400

    [UI] Add Basier Circle font

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,11 +9,16 @@ export default function LoginForm() {
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   return (
-    <main className="flex mx-auto mt-auto mb-0 center items-center justify-center rounded-lg border overflow-hidden border-gray-300">
-      <button onClick={() => signIn("google", { callbackUrl: "/" })} className="w-full p-2 bg-gray-300 rounded-lg hover:bg-gray-400 hover:text-white transition-color duration-200">
-        <GoogleIcon className="w-6 h-6" size={24} />
-        Log in with Google
-      </button>
+    <main className="flex flex-col mx-auto mt-auto mb-0 center items-center justify-center rounded-lg border overflow-hidden border-gray-300">
+      <div className="w-2/5 bg-grey-100 border-lg border border-gray-300 h-4/6 p-14">
+        <h2 className="text-lg">Please Log in to continue</h2>
+        <button
+          onClick={() => signIn("google", { callbackUrl: "/" })}
+          className="w-full p-2 bg-gray-300 flex rounded-lg hover:bg-gray-400 hover:text-white transition-color duration-200">
+          <GoogleIcon className="w-6 h-6 inline-block" size={24} />
+          Google
+        </button>
+      </div>
     </main>
   );
 }
